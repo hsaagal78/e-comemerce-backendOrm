52bd6dd32aab7e6fba42893588e23e9e0556bc74
hsaagal78 <hsagal78@gmail.com>
2023-07-13 19:13:06 -0400

    added import sequelize connection

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const category_routes = require('./api/category-routes');
+const produc_routes = require('./api/prodruct-routes');
+const tag_routes = require('./api/tag-routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// sync sequelize models to the database, then turn on the server. done
+db.sync({force: false})
+  .then(() => {});
+  // Start server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
