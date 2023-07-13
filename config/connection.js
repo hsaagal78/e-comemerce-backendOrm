d3a0d089ee13c8bee751113f31581b1bb7c8d8e1
hsaagal78 <hsagal78@gmail.com>
2023-07-13 18:28:05 -0400

    fist commit

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+const sequelize = new Sequelize('user_app_example', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+module.exports = sequelize;
